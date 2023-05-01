82567d0527efed21382478a0cc4f27e648c8158f
João Victor Garcia <joaovgs3000@gmail.com>
2023-05-01 17:00:13 -0300

    first commit

---
+++ src/app/projetin/projetin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projetin',
+  templateUrl: './projetin.component.html',
+  styleUrls: ['./projetin.component.css']
+})
+export class ProjetinComponent {
+
+}
